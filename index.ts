40cd01a825f9efe7a5d1f4891dcf82275a0ab8ee
marsnebulasoup <marsnebulasoup@gmail.com>
2023-03-05 21:50:45 -0600

    Added conversion to CSV

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { BroadcastEmailsFetcher } from './api';
+import { createWriteStream } from "fs"
+// @ts-ignore
+import { AsyncParser } from '@json2csv/node';
+
+const PATH = "test.csv";
+
+(async () => {
+  console.log("Fetching...");
+  const fetcher = new BroadcastEmailsFetcher; // pass token in
+  const emails = await fetcher.getFullBroadcastEmails();
+
+  const parser = new AsyncParser();
+  const stream = createWriteStream(PATH);
+  parser.parse(emails).pipe(stream);
+
+  console.log("\nComplete. Wrote", emails.length, "emails to", PATH);
+})()
